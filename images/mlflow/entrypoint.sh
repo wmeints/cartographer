ef77f93151be85f52d13a7a661223ca08ddb09b3
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-01-08 10:01:39 +0100

    Update MLFlow image to use one connection string

--- images/mlflow/entrypoint.sh
+++ images/mlflow/entrypoint.sh
@@ -2,12 +2,5 @@
 
 # Make sure we have sensible defaults for the registry and backend storage.
 MLFLOW_BACKEND_STORE="${MLFLOW_BACKEND_STORE:-file:///var/data/mlflow}"
-
-# You can switch of the registry by setting the MLFLOW_DISABLE_REGISTRY flag
-# By default, we assume that you use the registry as well as the metrics server.
-if [ -z "${MLFLOW_DISABLE_REGISTRY}" ]
-then
-  mlflow server --backend-store-uri "${MLFLOW_BACKEND_STORE}" --registry-store-uri "${MLFLOW_REGISTRY_STORE}"
-else 
-  mlflow server --backend-store-uri "${MLFLOW_BACKEND_STORE}"
-fi
+ 
+mlflow server --backend-store-uri "${MLFLOW_BACKEND_STORE}"
