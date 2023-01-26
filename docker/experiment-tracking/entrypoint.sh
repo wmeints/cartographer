bea1d8172cf8aaa99b8f08bcf4f10c95e0a05a79
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-01-26 21:42:53 +0100

    Encode password in database URL

--- docker/experiment-tracking/entrypoint.sh
+++ docker/experiment-tracking/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+ENCODED_PASSWORD=$(python -c "import urllib.parse; print(urllib.parse.quote_plus('${DB_PASS}'))")
+
 # Make sure we have sensible defaults for the registry and backend storage.
-MLFLOW_BACKEND_STORE="postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
+MLFLOW_BACKEND_STORE="postgresql://${DB_USER}:${ENCODED_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
  
 mlflow server --backend-store-uri "${MLFLOW_BACKEND_STORE}" --host 0.0.0.0
